45d784a2168033625f395331d790a77e6c4a51f7
Naitik Shah <n@daaku.org>
2021-11-13 12:29:41 +0400

    mark clojure dependency as provided, make v1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.clojars.daaku/metrics "0.0.3"
+(defproject org.clojars.daaku/metrics "1.0.0"
   :description "Clojure API over the Prometheus Java SDK."
   :url "https://github.com/daaku/metrics-clj"
   :scm {:name "git" :url "https://github.com/daaku/metrics-clj"}
   :license {:name "MIT License"}
   :dependencies [[io.prometheus/simpleclient "0.12.0"]
-                 [org.clojure/clojure "1.10.3"]])
+                 [org.clojure/clojure "1.10.3" :scope "provided"]])
